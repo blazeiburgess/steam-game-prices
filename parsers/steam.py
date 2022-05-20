c995e2f8b7f6eb0c06f35743da15a1a5cd05d0ef
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-20 06:24:07 -0400

    Make function name more descriptive

--- parsers/steam.py
+++ parsers/steam.py
@@ -4,7 +4,7 @@ class SteamParser(HTMLParser):
     def _remove_empty_values(self, results: list) ->list:
         return [val.strip() for val in results if val.strip()]
 
-    def _get_clean_value(self, results: list) -> str:
+    def _get_clean_price_values(self, results: list) -> str:
         clean_results = self._remove_empty_values(results)
         if results and clean_results:
             if len(clean_results) == 1:
@@ -21,7 +21,7 @@ class SteamParser(HTMLParser):
     def parse(self):
         records = []
         for row in self.etree.xpath("//a[contains(@class,'search_result_row')]"):
-            discount, price, discounted_price = self._get_clean_value(row.xpath('.//div[contains(@class,"search_price")]//text()'))
+            discount, price, discounted_price = self._get_clean_price_values(row.xpath('.//div[contains(@class,"search_price")]//text()'))
             record = {
                     'url': row.xpath('@href')[0].strip(),
                     'title': self._remove_empty_values(row.xpath('.//*[@class="title"]/text()'))[0],
