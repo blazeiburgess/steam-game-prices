14f9837f3669782bc60ed695267fcf162c7beabc
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-20 17:43:49 -0400

    Remove unnecessary url args, fix bug on operating system assignment, add log message

--- pullers/steam.py
+++ pullers/steam.py
@@ -1,14 +1,14 @@
 from constants import steam_tags
 from .base import BasePuller
 class SteamPuller(BasePuller):
-    PULL_URL_TEMPLATE = 'https://store.steampowered.com/search/results/?query={query}&start={offset}&tags={tags}&count=50&filter={page_type}&dynamic_data=&sort_by=_ASC&snr=1_7_7_230_7&infinite=1&os={os}'
+    PULL_URL_TEMPLATE = 'https://store.steampowered.com/search/results/?query={query}&start={offset}&tags={tags}&count=50&filter={page_type}&dynamic_data=&sort_by=_ASC&infinite=1&os={os}'
     base_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
         }
     def initialize(self, query, tags, operating_system, page_type, starting_offset, max_offset, *args, **kwargs):
         self.mapped_tags = []
         self.query = query
-        self.operating_system = operating_system,
+        self.operating_system = operating_system
         self.page_type = page_type
         self.starting_offset = starting_offset
         self.max_offset = max_offset
@@ -27,6 +27,7 @@ class SteamPuller(BasePuller):
                     page_type=self.page_type,
                     tags=",".join(self.mapped_tags),
                 )
+            self.log.info(f"Pulling url: {url}")
             page = self._get(url)
             resp = page.json()
             raw_html = resp['results_html']
