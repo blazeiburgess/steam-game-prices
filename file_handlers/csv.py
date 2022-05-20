7e7f2656cece3c672df11b5bbb7167da190dcd27
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-20 17:44:24 -0400

    Improve file naming convention to be more descriptive of the request

--- file_handlers/csv.py
+++ file_handlers/csv.py
@@ -1,15 +1,42 @@
 from csv import DictWriter
+from utils import parse_float
 class CSVWriter(object):
-    def __init__(self, data: dict, fieldnames: list, store: str, page_type: str, timestamp: int, *args, output_location: str='output', **kwargs):
-        self.data = data
+    def __init__(self, data: dict, fieldnames: list, store: str, page_type: str, timestamp: int, operating_system: str, 
+            query: str, tags: list, *args, output_location: str='output', **kwargs):
+        self.data = sorted(
+                data, 
+                key=lambda x: (
+                    int(
+                        str(x['discount'] or '0').replace('%','').replace('-','')
+                    ),
+                    parse_float(x['price']), 
+                ),
+                reverse=True
+            )
         self.fieldnames = fieldnames
 
+
         #TODO: make filename structure an argument
-        self.filename = f'{output_location}/{store}_{page_type}_{timestamp}.csv'
+        if operating_system:
+            os = f'{operating_system}_'
+        else:
+            os = ''
+        if query:
+            query = f'{query}_'
+        else:
+            query = ''
+
+        if tags:
+            tags = "--".join(list(tags)[:4]).replace(' ','_') + '_'
+        else:
+            tags = ''
+
+        self.filename = f'{output_location}/{store}_{page_type}_{os}{query}{tags}{timestamp}.csv'
 
 
     def write(self):
         with open(self.filename, 'w') as f:
-            writer = DictWrtier(fieldnames=self.fieldnames)
+            writer = DictWriter(f, fieldnames=self.fieldnames)
             writer.writeheader()
             writer.writerows(self.data)
+        return self.filename
