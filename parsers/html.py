d371effdbe3a547871abcea1f26e3d8327234e1b
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-20 17:40:41 -0400

    Add logger to html parser

--- parsers/html.py
+++ parsers/html.py
@@ -1,7 +1,10 @@
 from lxml.html import fromstring
+import logging
 class HTMLParser(object):
     def __init__(self, raw_html: str or bytes, *args, **kwargs):
         self.etree = fromstring(raw_html)
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.setLevel(logging.DEBUG)
 
         self.initialize(*args, **kwargs)
 
