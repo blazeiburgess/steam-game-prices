ee30d08d951122c3ebf0d08fa053baeec9f83f67
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-21 10:38:05 -0400

    Use country code in filename, rearrange some elements in filename

--- file_handlers/csv.py
+++ file_handlers/csv.py
@@ -2,15 +2,19 @@ from csv import DictWriter
 from utils import parse_float
 class CSVWriter(object):
     def __init__(self, data: dict, fieldnames: list, store: str, page_type: str, timestamp: int, operating_system: str, 
-            query: str, tags: list, *args, output_location: str='output', **kwargs):
+            query: str, tags: list, country_code: str, *args, output_location: str='output', **kwargs):
         self.data = sorted(
                 data, 
-                key=lambda x: (
+                key=lambda x: ( # TODO: make sort function controllable by user
                     int(
                         str(x['discount'] or '0').replace('%','').replace('-','')
                     ),
                     parse_float(x['price']), 
                 ),
+                # key=lambda x: (
+                #     x['parsed_price'] - parse_float(str(x['discounted_price'] or x['price'])),
+                #     x['parsed_price'], 
+                # ),
                 reverse=True
             )
         self.fieldnames = fieldnames
@@ -25,13 +29,17 @@ class CSVWriter(object):
             query = f'{query}_'
         else:
             query = ''
+        if country_code:
+            country_code = f'{country_code}_'
+        else:
+            country_code = ''
 
         if tags:
             tags = "--".join(list(tags)[:4]).replace(' ','_') + '_'
         else:
             tags = ''
 
-        self.filename = f'{output_location}/{store}_{page_type}_{os}{query}{tags}{timestamp}.csv'
+        self.filename = f'{output_location}/{store}_{os}{country_code}{page_type}_{query}{tags}{timestamp}.csv'
 
 
     def write(self):
