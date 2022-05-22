06e9e10abc4c7e1da6181b4b0597da4c427e0e9c
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-21 21:45:18 -0400

    Add farmed gog tags to constants

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,2 +1,3 @@
 from .steam_tags import steam_tags
 from .steam_countries import steam_countries
+from .gog_tags import gog_tags
---
+++ constants/gog_tags.py
@@ -0,0 +1,686 @@
+gog_tags = [
+  {
+    "name": "Fantasy",
+    "slug": "fantasy"
+  },
+  {
+    "name": "Open World",
+    "slug": "open-world"
+  },
+  {
+    "name": "First-Person",
+    "slug": "first-person"
+  },
+  {
+    "name": "Magic",
+    "slug": "magic"
+  },
+  {
+    "name": "Medieval",
+    "slug": "medieval"
+  },
+  {
+    "name": "Sandbox",
+    "slug": "sandbox"
+  },
+  {
+    "name": "Third Person",
+    "slug": "third-person"
+  },
+  {
+    "name": "Good Old Game",
+    "slug": "good-old-game"
+  },
+  {
+    "name": "Classic",
+    "slug": "classic"
+  },
+  {
+    "name": "Atmospheric",
+    "slug": "atmospheric"
+  },
+  {
+    "name": "Story Rich",
+    "slug": "story-rich"
+  },
+  {
+    "name": "Difficult",
+    "slug": "difficult"
+  },
+  {
+    "name": "Choices Matter",
+    "slug": "choices-matter"
+  },
+  {
+    "name": "Dark",
+    "slug": "dark"
+  },
+  {
+    "name": "Mature",
+    "slug": "mature"
+  },
+  {
+    "name": "Multiple Endings",
+    "slug": "multiple-endings"
+  },
+  {
+    "name": "Nudity",
+    "slug": "nudity"
+  },
+  {
+    "name": "FPS",
+    "slug": "fps"
+  },
+  {
+    "name": "Grand Strategy",
+    "slug": "grand-strategy"
+  },
+  {
+    "name": "Historical",
+    "slug": "historical"
+  },
+  {
+    "name": "Local Multiplayer",
+    "slug": "local-multiplayer"
+  },
+  {
+    "name": "Management",
+    "slug": "management"
+  },
+  {
+    "name": "Military",
+    "slug": "military"
+  },
+  {
+    "name": "Multiplayer",
+    "slug": "multiplayer"
+  },
+  {
+    "name": "Tactical",
+    "slug": "tactical"
+  },
+  {
+    "name": "Turn-Based",
+    "slug": "turn-based"
+  },
+  {
+    "name": "War",
+    "slug": "war"
+  },
+  {
+    "name": "Local Co-Op",
+    "slug": "local-co-op"
+  },
+  {
+    "name": "Exploration",
+    "slug": "exploration"
+  },
+  {
+    "name": "Base Building",
+    "slug": "base-building"
+  },
+  {
+    "name": "Crafting",
+    "slug": "crafting"
+  },
+  {
+    "name": "Resource Management",
+    "slug": "resource-management"
+  },
+  {
+    "name": "Sci-fi",
+    "slug": "sci-fi"
+  },
+  {
+    "name": "Space",
+    "slug": "space"
+  },
+  {
+    "name": "Survival",
+    "slug": "survival"
+  },
+  {
+    "name": "Great Soundtrack",
+    "slug": "great-soundtrack"
+  },
+  {
+    "name": "Cyberpunk",
+    "slug": "cyberpunk"
+  },
+  {
+    "name": "Science",
+    "slug": "science"
+  },
+  {
+    "name": "Post-apocalyptic",
+    "slug": "post-apocalyptic"
+  },
+  {
+    "name": "Dungeon Crawler",
+    "slug": "dungeon-crawler"
+  },
+  {
+    "name": "Horror",
+    "slug": "horror"
+  },
+  {
+    "name": "Stealth",
+    "slug": "stealth"
+  },
+  {
+    "name": "Underground",
+    "slug": "underground"
+  },
+  {
+    "name": "Hack and Slash",
+    "slug": "hack-and-slash"
+  },
+  {
+    "name": "Real-Time",
+    "slug": "real-time"
+  },
+  {
+    "name": "Lovecraftian",
+    "slug": "lovecraftian"
+  },
+  {
+    "name": "Survival Horror",
+    "slug": "survival-horror"
+  },
+  {
+    "name": "Gore",
+    "slug": "gore"
+  },
+  {
+    "name": "Violent",
+    "slug": "violent"
+  },
+  {
+    "name": "2D",
+    "slug": "two-dimensional"
+  },
+  {
+    "name": "Family Friendly",
+    "slug": "family-friendly"
+  },
+  {
+    "name": "Platformer",
+    "slug": "platformer"
+  },
+  {
+    "name": "Roguelike",
+    "slug": "roguelike"
+  },
+  {
+    "name": "Card Game",
+    "slug": "card-game"
+  },
+  {
+    "name": "Female Protagonist",
+    "slug": "female-protagonist"
+  },
+  {
+    "name": "Isometric",
+    "slug": "isometric"
+  },
+  {
+    "name": "Puzzle",
+    "slug": "puzzle"
+  },
+  {
+    "name": "Sexual Content",
+    "slug": "sexual-content"
+  },
+  {
+    "name": "Indie",
+    "slug": "indie"
+  },
+  {
+    "name": "Board Game",
+    "slug": "board-game"
+  },
+  {
+    "name": "Dinosaurs",
+    "slug": "dinosaurs"
+  },
+  {
+    "name": "Procedural Generation",
+    "slug": "procedural-generation"
+  },
+  {
+    "name": "Roguelite",
+    "slug": "roguelite"
+  },
+  {
+    "name": "crossplay",
+    "slug": "crossplay"
+  },
+  {
+    "name": "Hidden Object",
+    "slug": "hidden-object"
+  },
+  {
+    "name": "4X",
+    "slug": "four-x"
+  },
+  {
+    "name": "Modern",
+    "slug": "modern"
+  },
+  {
+    "name": "City builder",
+    "slug": "city-builder"
+  },
+  {
+    "name": "Funny",
+    "slug": "funny"
+  },
+  {
+    "name": "Point&Click",
+    "slug": "point-and-click"
+  },
+  {
+    "name": "Remake",
+    "slug": "remake"
+  },
+  {
+    "name": "Vampire",
+    "slug": "vampire"
+  },
+  {
+    "name": "Casual",
+    "slug": "casual"
+  },
+  {
+    "name": "Pixel Graphics",
+    "slug": "pixel-graphics"
+  },
+  {
+    "name": "Shoot'EmUp",
+    "slug": "shoot-em-up"
+  },
+  {
+    "name": "Supernatural",
+    "slug": "supernatural"
+  },
+  {
+    "name": "Top-Down",
+    "slug": "top-down"
+  },
+  {
+    "name": "Tabletop",
+    "slug": "tabletop"
+  },
+  {
+    "name": "Relaxing",
+    "slug": "relaxing"
+  },
+  {
+    "name": "Cartoony",
+    "slug": "cartoony"
+  },
+  {
+    "name": "LGBTQ+",
+    "slug": "lgbtq-plus"
+  },
+  {
+    "name": "NSFW",
+    "slug": "nsfw"
+  },
+  {
+    "name": "Logic",
+    "slug": "logic"
+  },
+  {
+    "name": "Crime",
+    "slug": "crime"
+  },
+  {
+    "name": "Investigation",
+    "slug": "investigation"
+  },
+  {
+    "name": "Mystery",
+    "slug": "mystery"
+  },
+  {
+    "name": "Noir",
+    "slug": "noir"
+  },
+  {
+    "name": "Thriller",
+    "slug": "thriller"
+  },
+  {
+    "name": "Psychological Horror",
+    "slug": "psychological-horror"
+  },
+  {
+    "name": "Only On GOG",
+    "slug": "only-on-gog"
+  },
+  {
+    "name": "Pirates",
+    "slug": "pirates"
+  },
+  {
+    "name": "FMV",
+    "slug": "fmv"
+  },
+  {
+    "name": "Surreal",
+    "slug": "surreal"
+  },
+  {
+    "name": "Zombies",
+    "slug": "zombies"
+  },
+  {
+    "name": "Dating Sim",
+    "slug": "dating-sim"
+  },
+  {
+    "name": "Fishing",
+    "slug": "fishing"
+  },
+  {
+    "name": "LifeSim",
+    "slug": "lifesim"
+  },
+  {
+    "name": "RTS",
+    "slug": "rts"
+  },
+  {
+    "name": "Fighting",
+    "slug": "fighting"
+  },
+  {
+    "name": "Flight",
+    "slug": "flight"
+  },
+  {
+    "name": "World War I",
+    "slug": "world-war-i"
+  },
+  {
+    "name": "Short",
+    "slug": "short"
+  },
+  {
+    "name": "CRPG",
+    "slug": "crpg"
+  },
+  {
+    "name": "Steampunk",
+    "slug": "steampunk"
+  },
+  {
+    "name": "Tactical RPG",
+    "slug": "tactical-rpg"
+  },
+  {
+    "name": "Dystopian",
+    "slug": "dystopian"
+  },
+  {
+    "name": "Psychological",
+    "slug": "psychological"
+  },
+  {
+    "name": "Philosophical",
+    "slug": "philosophical"
+  },
+  {
+    "name": "Walking Simulator",
+    "slug": "walking-simulator"
+  },
+  {
+    "name": "World War II",
+    "slug": "world-war-ii"
+  },
+  {
+    "name": "Western",
+    "slug": "western"
+  },
+  {
+    "name": "Metroidvania",
+    "slug": "metroidvania"
+  },
+  {
+    "name": "Robots",
+    "slug": "robots"
+  },
+  {
+    "name": "Emotional",
+    "slug": "emotional"
+  },
+  {
+    "name": "JRPG",
+    "slug": "jrpg"
+  },
+  {
+    "name": "Politics",
+    "slug": "politics"
+  },
+  {
+    "name": "Souls-like",
+    "slug": "souls-like"
+  },
+  {
+    "name": "Puzzle Platformer",
+    "slug": "puzzle-platformer"
+  },
+  {
+    "name": "Nature",
+    "slug": "nature"
+  },
+  {
+    "name": "Split Screen",
+    "slug": "split-screen"
+  },
+  {
+    "name": "Visual Novel",
+    "slug": "visual-novel"
+  },
+  {
+    "name": "Trading",
+    "slug": "trading"
+  },
+  {
+    "name": "Transportation",
+    "slug": "transportation"
+  },
+  {
+    "name": "Realistic",
+    "slug": "realistic"
+  },
+  {
+    "name": "Parody",
+    "slug": "parody"
+  },
+  {
+    "name": "LevelEditor",
+    "slug": "leveleditor"
+  },
+  {
+    "name": "Tanks",
+    "slug": "tanks"
+  },
+  {
+    "name": "Party Game",
+    "slug": "party-game"
+  },
+  {
+    "name": "Beat 'em up",
+    "slug": "beat-em-up"
+  },
+  {
+    "name": "Romance",
+    "slug": "romance"
+  },
+  {
+    "name": "Mythology",
+    "slug": "mythology"
+  },
+  {
+    "name": "Cats",
+    "slug": "cats"
+  },
+  {
+    "name": "Deckbuilding",
+    "slug": "deckbuilding"
+  },
+  {
+    "name": "Choose Your Own Adventure",
+    "slug": "choose-your-own-adventure"
+  },
+  {
+    "name": "Dog",
+    "slug": "dog"
+  },
+  {
+    "name": "Naval",
+    "slug": "naval"
+  },
+  {
+    "name": "Physics",
+    "slug": "physics"
+  },
+  {
+    "name": "Trains",
+    "slug": "trains"
+  },
+  {
+    "name": "Tower Defense",
+    "slug": "tower-defense"
+  },
+  {
+    "name": "Bullet Hell",
+    "slug": "bullet-hell"
+  },
+  {
+    "name": "Perma Death",
+    "slug": "perma-death"
+  },
+  {
+    "name": "VR",
+    "slug": "vr"
+  },
+  {
+    "name": "Football",
+    "slug": "football"
+  },
+  {
+    "name": "Soccer",
+    "slug": "soccer"
+  },
+  {
+    "name": "Dark Comedy",
+    "slug": "dark-comedy"
+  },
+  {
+    "name": "Text-Based",
+    "slug": "text-based"
+  },
+  {
+    "name": "Education",
+    "slug": "education"
+  },
+  {
+    "name": "Twin Stick Shooter",
+    "slug": "twin-stick-shooter"
+  },
+  {
+    "name": "Petting Dogs",
+    "slug": "petting-dogs"
+  },
+  {
+    "name": "Vikings",
+    "slug": "vikings"
+  },
+  {
+    "name": "Non Violent",
+    "slug": "non-violent"
+  },
+  {
+    "name": "building",
+    "slug": "building"
+  },
+  {
+    "name": "Hentai",
+    "slug": "hentai"
+  },
+  {
+    "name": "Ninja",
+    "slug": "ninja"
+  },
+  {
+    "name": "Underwater",
+    "slug": "underwater"
+  },
+  {
+    "name": "Martial Arts",
+    "slug": "martial-arts"
+  },
+  {
+    "name": "Cooking",
+    "slug": "cooking"
+  },
+  {
+    "name": "Werewolves",
+    "slug": "werewolves"
+  },
+  {
+    "name": "Rhythm",
+    "slug": "rhythm"
+  },
+  {
+    "name": "Superhero",
+    "slug": "superhero"
+  },
+  {
+    "name": "Demo",
+    "slug": "demo"
+  },
+  {
+    "name": "Sniper",
+    "slug": "sniper"
+  },
+  {
+    "name": "Farming",
+    "slug": "farming"
+  },
+  {
+    "name": "Typing",
+    "slug": "typing"
+  },
+  {
+    "name": "Time Manipulation",
+    "slug": "time-manipulation"
+  },
+  {
+    "name": "Pinball",
+    "slug": "pinball"
+  },
+  {
+    "name": "Match3",
+    "slug": "match-three"
+  },
+  {
+    "name": "Looter Shooter",
+    "slug": "looter-shooter"
+  },
+  {
+    "name": "Escape Room",
+    "slug": "escape-room"
+  },
+  {
+    "name": "Movie",
+    "slug": "movie"
+  },
+  {
+    "name": "Auto Battler",
+    "slug": "auto-battler"
+  }
+]
