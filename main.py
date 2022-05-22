b0b98f6b7f83a20f3411d8ff3b50d7bff51ac22c
Blaze Burgess <blaze.i.burgess@gmail.com>
2022-05-21 21:44:28 -0400

    Simplify main method, add GOG as possible execution option

--- main.py
+++ main.py
@@ -5,45 +5,23 @@ from utils import random_sleep
 from datetime import datetime
 from file_handlers import CSVWriter
 from constants import steam_tags, steam_countries
+from enums import StoreEnum
 import logging
 from lxml.etree import ParserError
 
 def main():
-    log = logging.getLogger('__main__')
     argparser = ArgParser()
     args = argparser.parse()
-    if args['list_steam_tags'] is True:
-        return print("\n".join(
-            sorted(
-                [key.replace(' ','_') for key in steam_tags.keys()]
-            )
-        ))
-    if args['list_steam_countries'] is True:
-        lines = []
-        for country in sorted(steam_countries, key=lambda x: x['code']):
-            lines.append("{code}   | {name}".format(**country))
-        print("ISO2 | Name\n----------------------")
-        return print("\n".join(lines))
-    if args['tags']:
-        args['tags'] = set(args['tags'])
+    if args['game_store'] == StoreEnum.STEAM.value:
+        from stores import SteamStore as Store
+    elif args['game_store'] == StoreEnum.GOG.value:
+        from stores import GogStore as Store
+    else:
+        raise ValueError(f'Store not supported: {args["game_store"]}')
 
-    pull_timestamp = int(datetime.timestamp(datetime.utcnow()))
+    store = Store(args)
+    store.run()
 
-    puller = SteamPuller(**args)
-    results = []
-    for raw_html_page in puller.pull():
-        try:
-            page_parser = SteamParser(raw_html_page)
-        except ParserError as pe:
-            log.warning("Empty html page was returned from puller. Assuming there are no more results to pull")
-            break
-        results.extend(page_parser.parse())
-
-        random_sleep(args['random_sleep_min'], args['random_sleep_max'])
-
-    writer = CSVWriter(results, results[0].keys(), 'steam', args['page_type'], pull_timestamp, args['operating_system'],
-            args['query'], args['tags'], args['country_code'])
-    writer.write()
 
 if __name__ == '__main__':
     main()
